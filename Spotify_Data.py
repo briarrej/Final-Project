ffae7ee54af3737aad25c816f8d1a54b6dd49879
Morgan Miller <morganmiller@Morgans-Air.lan>
2021-04-17 17:27:57 -0400

    added spotipy

--- Spotify_Data.py
+++ Spotify_Data.py
@@ -6,3 +6,32 @@ import csv
 import unittest
 import json
 import sqlite3
+import spotipy.util as util
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+def getSpotifyLink():
+    url = 'https://spotifycharts.com/regional'
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    getSpotifyLink()
+    cur, conn = setUpDatabase('Spotify.db')
+    createDatabase(cur, conn)
+
+main()
